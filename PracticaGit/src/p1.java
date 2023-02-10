ebb6cfe3cb0708b4ad7d40cc8c9767d23e78c8b9
Adrian <xadriantristanx@gmail.com>
2023-02-10 19:37:13 +0100

    Cambios en rama principal.

--- PracticaGit/src/p1.java
+++ PracticaGit/src/p1.java
@@ -4,6 +4,6 @@ public class p1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Con cambios en rama principal");
-			int principal = 777;
+			int principal = 727;
 	}
 }
